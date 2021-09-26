86812348441a67bba4e0ba93f9c6e857cb58a430
MYSTERY INSIDE <aarti>
2021-09-26 16:03:27 -0700

    final

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -162,8 +162,9 @@ EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST="smtp.gmail.com"
 EMAIl_PORT=587
 EMAIL_HOST_USER="aartikumarisingh120@gmail.com"
-EMAIL_HOST_PASSWORD="bihar02aarti"
+EMAIL_HOST_PASSWORD="furormlqxgsyrody"
 EMAIL_USE_TLS=True
-DEFAULT_FROM_EMAIL="Student management System <aartikumarisingh120@gmail.com>"
+DEFAULT_FROM_EMAIL="Student management System <noreply@studentmanagementsystem.com>"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+#furormlqxgsyrody
